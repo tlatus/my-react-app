127d9158e346716a8b0bace8e6a10701be3b6869
Thomas Latus <thomaslatus@Thomass-MacBook-Pro.local>
2018-07-26 12:45:04 +0300

    somtheing new

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn how to design and code React apps</h1>
+    <p>Complete Courses Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
