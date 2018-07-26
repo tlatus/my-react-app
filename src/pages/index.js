0d34e17bdac6a72376c9742429000066569c79fd
Thomas Latus <latus@modulr.design>
2018-07-26 14:30:24 +0300

    ab gehts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn how to design and code React apps</h1>
-    <p>Complete Courses Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn how to design and code React apps</h1>
+        <p>Complete Courses Nullam quis risus eget urna mollis ornare vel eu leo. </p>
+        <Link to="/page-2/">Watch the Video</Link>
+      </div>
+    </div>
   </div>
 )
 
